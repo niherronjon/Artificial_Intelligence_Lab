94047470a87e34dffbf054a49709440d974fa694
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-10-30 12:20:27 +0600

    Create Lab_1.py

---
+++ Lab_1/Lab_1.py
@@ -0,0 +1,49 @@
+print("Hello AI")
+
+
+
+name = 5
+Name = "Lab"
+print(name)
+print(Name)
+
+
+age=32
+if age>30:
+ age2=34
+ print(age2)
+
+print(age)
+
+
+
+
+x=10;
+y=20;
+z=x+y
+print(z)
+
+
+
+
+ height=5.11
+ type(height)
+
+float(int(height))
+
+
+
+num1=float(input("enter first num: "))
+num2=float(input("enter second num:"))
+sum=num1+num2
+print(sum)
+
+
+
+
+num1=float(input("enter first num: "))
+num2=float(input("enter second num:"))
+sum=num1+num2
+
+print("sum: ",sum)
+print("Difference: ",num1-num2)
